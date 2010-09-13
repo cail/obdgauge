8a205a46426a4dc8ec93ba4d265d2bf5b32577ac
Igor Russkih <irusskih@gmail.com>
2010-09-13 12:52:19 +0400

    cm&git stuff

---
+++ build2.bat
@@ -0,0 +1,3 @@
+C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Platform=AnyCPU OBDGauge.csdproj
+pause
+"C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug/OBDGauge.exe "dev:/SDMMC/WinCEdev/OBDGauge mod"
